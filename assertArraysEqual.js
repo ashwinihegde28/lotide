de6d7b2ddf257e271583fa6b22c5184493b39ad1
ashwinihegde28 <ashwinihegde28@gmail.com>
2023-03-07 11:02:43 -0500

    Code for assertArraysEqual.js added

---
+++ assertArraysEqual.js
@@ -0,0 +1,26 @@
+
+// Implementing assertArraysEqual which will take in two arrays and console.log an appropriate message to the console using previously implemented "eqArrays" function
+const eqArrays = function(arr1 , arr2) {
+  let isTrue = false;
+  if (arr1.length !== arr1.length) {
+    return false;
+  }
+  for (let i in arr1) {
+    (arr1[i] === arr2[i]) ? isTrue = true : isTrue = false;
+  }
+  console.log(isTrue);
+  return isTrue;
+};
+
+const assertArraysEqual = function(actual , expected) {
+  const happySmiley = String.fromCodePoint(0x1F600);
+  const sadSmiley = String.fromCodePoint(0x1F614);
+  const isEqual = eqArrays(actual , expected);
+  // include template literal to display
+  let match = `${happySmiley} Assertion Passed: ${actual} == ${expected}`;
+  let misMatch = `${sadSmiley} Assertion Failed: ${actual}  !== ${expected}`;
+  (isEqual) ? match : misMatch;
+};
+
+assertArraysEqual([5,6,7] , [5,6,7]);
+assertArraysEqual(['a','b'] , ['a','c']);
