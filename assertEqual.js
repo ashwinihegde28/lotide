e7c0aa31a7c274008b795c2ead870a5a26523aa3
ashwinihegde28 <ashwinihegde28@gmail.com>
2023-03-06 20:06:14 -0500

    Update the file with template string

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  const happySmiley = String.fromCodePoint(0x1F600);
+  const sadSmiley = String.fromCodePoint(0x1F614);
+  // include template literal to display
+  let match = `${happySmiley} Assertion Passed: ${actual} == ${expected}`;
+  let mismatch = `${sadSmiley} Assertion Failed: ${actual}  !== ${expected}`;
+  (actual === expected) ? console.log(match) : console.log(mismatch);
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
