a729871eef71057233d4e8ad3ebb3409ff8f55b4
ashwinihegde28 <ashwinihegde28@gmail.com>
2023-03-06 20:56:05 -0500

    created the file and updated head.js

---
+++ head.js
@@ -0,0 +1,19 @@
+const assertEqual = function(actual, expected) {
+    
+    if(isNaN(actual) || expected.length < 0  || actual.length < 0 || isNaN(expected)){   
+        return;
+    }           
+    const happySmiley = String.fromCodePoint(0x1F600);
+    const sadSmiley = String.fromCodePoint(0x1F614);
+    // include template literal to display
+    let match = `${happySmiley} Assertion Passed: ${actual} == ${expected}`;
+    let mismatch = `${sadSmiley} Assertion Failed: ${actual}  !== ${expected}`;
+    (actual === expected) ? console.log(match) : console.log(mismatch);
+    
+  };
+  
+const head = (array) => 
+(array.length < 1)?console.log("Undefined"): array[0];
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), []);
